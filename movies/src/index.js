37b5d9c10c48f2a64711a64f6302023f35c56eb6
Ling Feng <fengling1964@gmail.com>
2022-10-19 01:21:09 +0100

    E2E tests - App Base test cases.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
